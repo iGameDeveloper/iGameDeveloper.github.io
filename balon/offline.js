a2f04775c64c4285009d3d5eeb326a5da04e6639
Jonasz <delgado88a@o2.pl>
2016-10-18 22:46:52 +0200

    balon

---
+++ balon/offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1476823591,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/startbutton-sheet0.png",
+		"images/www-sheet0.png",
+		"images/tiledbackground.png",
+		"images/www2-sheet0.png",
+		"images/www3-sheet0.png",
+		"images/www4-sheet0.png",
+		"images/www5-sheet0.png",
+		"images/rawbutton-sheet0.png",
+		"images/topdefault-sheet0.png",
+		"images/downsheet-sheet0.png",
+		"images/balonmetalizowanyblekit-sheet0.png",
+		"images/tr1-sheet0.png",
+		"images/tr2-sheet0.png",
+		"images/tr3-sheet0.png",
+		"images/strona-sheet0.png",
+		"images/strona2-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
