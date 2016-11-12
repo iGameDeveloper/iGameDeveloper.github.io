c23bbe1d401c319bbb8378be238d6e8dad1bad39
Jonasz <delgado88a@o2.pl>
2016-11-13 00:21:59 +0100

    f

---
+++ balon/Majery Memory/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1478519471,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/card-sheet0.png",
+		"images/card-sheet1.png",
+		"images/card-sheet2.png",
+		"images/tlo-sheet0.png",
+		"images/menu-sheet0.png",
+		"images/wyniki-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
