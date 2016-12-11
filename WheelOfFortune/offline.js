399dfcd9d93a3846c5847c8bc6365444c696c285
Jonasz <delgado88a@o2.pl>
2016-12-12 00:55:55 +0100

    h

---
+++ WheelOfFortune/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1481500510,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"media/button_1.m4a",
+		"media/button_1.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
