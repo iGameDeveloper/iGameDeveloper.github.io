7de5c3055be47216e4e825822aa60086bfeb112f
Jonasz <delgado88a@o2.pl>
2017-01-20 20:44:27 +0100

    d

---
+++ Zbieranie/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1484941390,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/drzewo-sheet0.png",
+		"images/owoce-sheet0.png",
+		"images/owoce-sheet1.png",
+		"images/owoce-sheet2.png",
+		"media/106130__j1987__leafpilehit.m4a",
+		"media/106130__j1987__leafpilehit.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
