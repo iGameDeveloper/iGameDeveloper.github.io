3ad8d36ff89c499485026cb3ab7402e23f3cd21e
Jonasz <delgado88a@o2.pl>
2017-02-24 15:54:52 +0100

    dfdf

---
+++ Rankinggraczy/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1487948075,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/rankingnajlepszychgrac-sheet0.png",
+		"images/tył-default-000.jpg",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"firebase.js",
+		"animate.min.css"
+	]
+}
