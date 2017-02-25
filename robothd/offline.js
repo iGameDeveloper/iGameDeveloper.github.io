cda8e39314b395215a81e748eba86ec126c84db1
Jonasz <delgado88a@o2.pl>
2017-02-25 17:09:57 +0100

    f

--- robothd/offline.js
+++ robothd/offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1488028533,
+	"version": 1488038979,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -7,6 +7,9 @@
 		"offlineClient.js",
 		"images/tło-default-000.jpg",
 		"images/anim-sheet0.png",
+		"images/anim-sheet1.png",
+		"images/cel-sheet0.png",
+		"images/tiledbackground.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
