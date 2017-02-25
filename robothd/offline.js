37617d4a46a79d95f6f82648036506b34d7e79e8
Jonasz <delgado88a@o2.pl>
2017-02-25 14:12:11 +0100

    r

--- robothd/offline.js
+++ robothd/offline.js
@@ -1,11 +1,11 @@
 ﻿{
-	"version": 1488028108,
+	"version": 1488028320,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
-		"images/tło-sheet0.png",
+		"images/tło-default-000.jpg",
 		"images/anim-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
