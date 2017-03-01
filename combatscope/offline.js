46e360d9f45f4b94cc802e72b96bc09cd4d2177b
Jonasz <delgado88a@o2.pl>
2017-03-01 20:48:11 +0100

    h

---
+++ combatscope/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1488397664,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/scopebase-sheet0.png",
+		"images/scopecrosshair-sheet0.png",
+		"images/bullethole-sheet0.png",
+		"media/gun.m4a",
+		"media/gun.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
