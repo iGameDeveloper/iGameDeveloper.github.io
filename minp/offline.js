0464ecbffb277e37e16bbb70e30226687750efda
Jonasz <delgado88a@o2.pl>
2017-04-03 13:47:12 +0200

    d

---
+++ minp/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1491219971,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/platforma-sheet0.png",
+		"images/key-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/tiledbackground.png",
+		"media/pickup_coin.m4a",
+		"media/pickup_coin.ogg",
+		"media/hit_hurt3.m4a",
+		"media/hit_hurt3.ogg",
+		"media/click.m4a",
+		"media/click.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
