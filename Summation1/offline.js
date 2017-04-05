8563aacd8bf68e9192f28c447a4b4841b0478d5c
Jonasz <delgado88a@o2.pl>
2017-04-05 11:44:59 +0200

    v

---
+++ Summation1/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1491385475,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/playerbullet-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/hud.png",
+		"images/tiledbackground.png",
+		"media/score.m4a",
+		"media/score.ogg",
+		"media/level end 2.m4a",
+		"media/level end 2.ogg",
+		"media/hit.m4a",
+		"media/hit.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
