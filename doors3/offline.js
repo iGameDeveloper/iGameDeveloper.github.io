4794f7687f1aaf41b3c7ad013e78f9d26b9a61a2
Jonasz <delgado88a@o2.pl>
2017-04-24 17:59:51 +0200

    k

---
+++ doors3/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1493049542,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/doorleftmain-sheet0.png",
+		"images/1player-sheet0.png",
+		"images/wall.png",
+		"images/doorright-sheet0.png",
+		"media/error_001.m4a",
+		"media/error_001.ogg",
+		"media/opendoor.m4a",
+		"media/opendoor.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
