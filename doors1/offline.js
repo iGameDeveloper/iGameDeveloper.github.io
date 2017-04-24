308b0dc950f5a4f8de4307bbf6f6a899e90428c7
Jonasz <delgado88a@o2.pl>
2017-04-24 15:11:00 +0200

    d1

---
+++ doors1/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1493039407,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/doorleftmain-sheet0.png",
+		"images/1player-sheet0.png",
+		"images/tiledbackground.png",
+		"images/doorright-sheet0.png",
+		"media/componentdropintoinv.m4a",
+		"media/componentdropintoinv.ogg",
+		"media/componentdropintospace.m4a",
+		"media/componentdropintospace.ogg",
+		"media/error_001.m4a",
+		"media/error_001.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
