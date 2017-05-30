aefcb24d638bfdb0b7968b2500b376343f9d57b3
Jonasz <delgado88a@o2.pl>
2017-05-30 21:37:07 +0200

    d

---
+++ Parking/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1496172975,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/intro-sheet0.png",
+		"images/background-sheet0.png",
+		"images/car-sheet0.png",
+		"images/wall.png",
+		"images/det-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
